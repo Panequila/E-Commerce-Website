94a50d82df66b719f42c9b946403cfb32f470567
amirNazeh <130168914+amirNazeh@users.noreply.github.com>
2023-04-14 00:26:01 +0200

    footer-responsive

--- script.js
+++ script.js
@@ -1,1 +1,16 @@
 // Script for navigation bar
+const bar = document.getElementById("bar");
+const clos = document.getElementById("close");
+const nav= document.getElementById("navbar");
+
+if(bar){
+    bar.addEventListener('click',function(){
+        nav.classList.add("active");
+    })
+}
+
+if(clos){
+    clos.addEventListener('click',function(){
+        nav.classList.remove("active");
+    })
+}
